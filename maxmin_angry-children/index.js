9213067b63821046dde7ac9d7a0919528dacdbf6
Rafay <rafay@192.168.1.2>
2024-04-28 12:21:25 -0700

    min-max previosuly labeled as angry-children solution

---
+++ maxmin_angry-children/index.js
@@ -0,0 +1,43 @@
+// Initial Solution
+
+/** Thought Process
+ *  - have to make sub array of the given length
+ *  - and need to compare max - min , and need to return the minimal difference value
+ *
+ *  - fist sort down the array, the substring should be consisit of consectutive element
+ *  - declare minimum value for first subsring
+ *  - check the difference and store the minumum difference
+ *
+ */
+
+// function maxMin(k, arr) {
+//   // Write your code here
+//   arr.sort((a, b) => a - b);
+
+//   let min = arr[k - 1] - arr[0];
+//   for (let i = 0; i < arr.length; i++) {
+//     const diff = Math.abs(arr[k + i - 1] - arr[i]);
+//     if (diff < min) {
+//       min = diff;
+//     }
+//   }
+//   return min;
+// }
+
+//Final Solution 
+
+/**
+ * Thought Process
+ * - Same as above , but instead of if condition 
+ * - Math.min method is used to compare
+ */
+function maxMin(k, arr) {
+  // Write your code here
+  arr.sort((a, b) => a - b);
+
+  let min = arr[k - 1] - arr[0];
+
+  for (let i = 0; i < arr.length - k + 1; i++) {
+    min = Math.min(min, arr[k + i - 1] - arr[i]);
+  }
+}
